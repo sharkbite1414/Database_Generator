c74bbaffa0fa24751ed2e86908a1f54dc47c1ff9
sharkbite1414 <sharkbite1414@gmail.com>
2015-08-08 16:05:54 +0100

    Updated README.md Output to csv file now works. Floats can now be used for minimum and maximum. Random data generation works again.

--- DatabaseGenerator.py
+++ DatabaseGenerator.py
@@ -1,23 +1,24 @@
 import random
 from statistics import median
 import json
-#import csv
+import csv
 
 
 data_values = []
 
 
-def random_data(n, minimum, maximun):
-  for x in random.sample(range(minimum,maximun),n):
-    data_values.append(x)
+def random_data(n, minimum, maximum):
+  for x in range(0, n):
+    r = random.uniform(minimum, maximum)
+    data_values.append(r)
   return data_values
 
-def trending_data(n, minimum, maximun):
+def trending_data(n, minimum, maximum):
   i = n
   data_values.append(minimum)
   while i > 0:
     k = n-i
-    data_values.append(data_values[k] + random.randint(-2,5)) #TO_DO randint should be tied to maximun and minimum
+    data_values.append(data_values[k] + random.randint(-2,5)) #TO_DO randint should be tied to maximum and minimum
     i -= 1
   return data_values
 
@@ -29,7 +30,7 @@ def normal_data(n, mu, sigma):
   return data_values
 
 
-print ("shark's database generator v0.002")
+print ("sharkbite1414's database generator v0.002")
 
 n = int(input ("Number of samples (n) = "))
 print ("n =",n)
@@ -37,18 +38,18 @@ print ("n =",n)
 data_points = list(range(1,n+1))
 print (data_points)
 
-data_type = input ("Choose data type: (R)andom, (T)rending, (N)ormal.")
+data_type = input ("Choose data type: (R)andom, (T)rending, (N)ormal: ")
 if data_type == "R" or data_type == "r":
-  minimum = int(input("Choose minimum data value: "))
-  maximun = int(input("Choose maximun data value: "))
-  random_data(n, minimum, maximun)
+  minimum = float(input("Choose minimum data value: "))
+  maximum = float(input("Choose maximum data value: "))
+  random_data(n, minimum, maximum)
 elif data_type == "T" or data_type == "t":
-  minimum = int(input("Choose minimum data value: "))
-  maximun = int(input("Choose maximun data value: "))
-  trending_data(n, minimum, maximun)
+  minimum = float(input("Choose minimum data value: "))
+  maximum = float(input("Choose maximum data value: "))
+  trending_data(n, minimum, maximum)
 elif data_type == "N" or data_type == "n":
-  mu = int(input("Choose mean: "))
-  sigma = int(input("Choose std dev: "))
+  mu = float(input("Choose mean: "))
+  sigma = float(input("Choose std dev: "))
   normal_data(n, mu, sigma)
 
 
@@ -63,10 +64,9 @@ print (database)
 with open("output.json","w") as json_file:
   json_file.write(json.dumps(database))
 
-#with open("output.csv", "w", newline="") as csvfile:
-  #csv_file = csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
-  #csv_file.writerow(database.keys())
-  #csv_file.writerow(database.values())
+with open("output.csv", "w") as csv_file:
+  w = csv.writer(csv_file)
+  w.writerows(database.items())
 
-print ("Data has been exported to output.json")
-input("Press Enter to close")
+
+print ("Data has been exported to output.json and output.csv")
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# DATBASE GENERATOR
-This project is currently incomplete and mostly broken.
-Run 'python3 Database generator.py'
+# DATABASE GENERATOR
+
+1. Run 'python3 Database generator.py' from the command line.
+2. Enter number samples (data points). This must be an integer.
+3. Select type of data to generate.
+  * Random - produces random data
+  * Trending - produces data that trends from low to high.
+  * Normal - produces a random normal distribution.
+4. Enter parameters:
+  * Random and Trending asks for minimum and maximum. These can be floats eg. 2.2
+  * Normal asks for mean and standard deviation. These can be floats eg. 4.5
+5. The database is outputted to output.csv in the same directory as DatabaseGenerator.py.
